50fa161d4c08b81231d194a92827881ca70f142f
Davis-Xtranet <davis.xtranet.co.ke>
2021-06-03 16:15:35 +0300

    Them colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+final bgColor = Color.fromRGBO(101, 204, 252, 1);
+final whiteColor = Colors.white;
+final darkText = Color.fromRGBO(6, 65, 93, 1);
+final bgRedColor = Color.fromRGBO(255, 97, 97, 1);
